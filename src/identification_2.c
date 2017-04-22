130633a3cf318e7f2df1ff9a86c4729ebfd11096
Anhelina Shulha <ashulha@e1z2r8p7.42.us.org>
2017-04-22 16:59:22 -0700

    still have lots of work to do..

--- src/identification_2.c
+++ src/identification_2.c
@@ -71,7 +71,7 @@ char	*ft_pg(va_list *ap, flags *f, length *l)
 	union data	type;
 	char		*s;
 
-	type.d = (l->L = 1) ? (long double)va_arg(*ap, double) : va_arg(*ap, double);
+	type.ld = (l->L = 1) ? (long double)va_arg(*ap, double) : va_arg(*ap, double);
 	if (type.d == 0.0 && f->precision == 0)
 		return (ft_strnew(0));
 	if (f->precision == -1)
